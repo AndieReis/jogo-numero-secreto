34db0870dfeff5c4ea83e4f614343207ab56879a
Andie Reis <andie.reis@gmail.com>
2024-03-08 09:53:27 -0300

    Incrementando a lógica do jogo.

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-alert("Sejam bem vindos ao Jogo do Número Secreto");
+alert("Sejam bem vindos ao Jogo do Número Secreto!");
 
-let numeroSecreto = 5;
-let chute = prompt("Escolha um número de 1 a 10: ");
 
-if (chute == numeroSecreto) {
-    console.log(`Você acertou! O número secreto é: ${numeroSecreto}`);
-} else {
-    if (chute < numeroSecreto) {
-        console.log(`Você errou, o número secreto é maior que ${chute}.`);
+let numeroMaximo = 100;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto);
+let chute
+let tentativas = 1;
+
+while (chute != numeroSecreto) {
+    chute = prompt(`Escolha um número de 1 a ${numeroMaximo}: `);
+
+    if (chute == numeroSecreto) {
+        break;
     } else {
-        console.log(`Você errou, o número secreto é menor que ${chute}.`);
+        if (chute < numeroSecreto) {
+            alert(`Você errou, o número secreto é maior que ${chute} .`);
+        } else {
+            alert(`Você errou, o número secreto é menor que ${chute} .`);
+        }
     }
+    tentativas++;
 }
+
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+alert(`Você acertou! O número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa} .`);
+
+
